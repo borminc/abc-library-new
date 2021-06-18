0da518d0eb57187effe3568efb0092bfca5ebaf2
borminc <83851923+borminc@users.noreply.github.com>
2021-06-18 15:05:01 +0700

    trying to fix merge

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::view('/{path?}/{path1?}/{path2?}/{path3?}/{path4?}', 'index');
+Route::view('/{path?}', 'index');
 
 
