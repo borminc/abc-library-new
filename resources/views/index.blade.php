d95f96f1d58b52492db8df0247589e3972c1122c
borminc <83851923+borminc@users.noreply.github.com>
2021-07-07 22:05:54 +0700

    extract vendor js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,8 @@
     </head>
     <body class="antialiased">
         <div id="index"></div>
+        <script src="{{ asset('js/manifest.js') }}"></script>
+        <script src="{{ asset('/js/vendor.js') }}"></script>
         <script src="{{ asset('js/app.js') }}"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </body>
